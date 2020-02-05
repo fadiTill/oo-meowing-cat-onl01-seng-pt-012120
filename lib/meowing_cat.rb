30e01f9e8b3427cf0d7b6d31f05f021086e72912
fadimata tillman <fadi.tillman@gmail.com>
2020-02-05 23:11:19 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,9 +1,9 @@
 ## code your solution here.
-class Cat
+class Cat(name,sound)
   attr_accessor:meow
   attr_accessor:name
 end
 
-cat.new = maru
+cat.new = "maru"
 maru.name = "Maru"
 maru.meow = "meow"
